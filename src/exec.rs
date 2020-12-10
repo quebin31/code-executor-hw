653e9cffbaee5983461650217e8376a035a258bf
Kevin Del Castillo <quebin31@gmail.com>
2020-12-10 13:20:58 -0500

    Move logic code to exec module

--- src/exec.rs
+++ src/exec.rs
@@ -5,29 +5,29 @@ use std::process::{Command, Output};
 
 use crate::error::Error;
 
-#[derive(Debug, Clone, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum ExecCode {
     Line(String),
     Multi(Vec<String>),
 }
 
-#[derive(Debug, Clone, Deserialize)]
-pub struct ExecInput {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ExecRequest {
     pub code: ExecCode,
 }
 
-#[derive(Debug, Clone, Serialize)]
-pub struct ExecOutput {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ExecResponse {
     pub stdout: String,
     pub stderr: String,
     pub code: Option<i32>,
     pub signal: Option<i32>,
 }
 
-impl From<Output> for ExecOutput {
+impl From<Output> for ExecResponse {
     fn from(output: Output) -> Self {
-        ExecOutput {
+        ExecResponse {
             stdout: String::from_utf8_lossy(&output.stdout).to_string(),
             stderr: String::from_utf8_lossy(&output.stderr).to_string(),
             code: output.status.code(),
@@ -36,7 +36,20 @@ impl From<Output> for ExecOutput {
     }
 }
 
-pub fn python(code: &str) -> Result<ExecOutput, Error> {
+pub fn exec_req(req: &ExecRequest) -> Result<ExecResponse, Error> {
+    let code = match &req.code {
+        ExecCode::Line(line) => line.to_owned(),
+        ExecCode::Multi(lines) => {
+            let mut code = String::new();
+            for line in lines {
+                code.push_str(&line);
+                code.push('\n');
+            }
+
+            code
+        }
+    };
+
     let output = Command::new("python").arg("-c").arg(code).output()?;
     Ok(output.into())
 }
